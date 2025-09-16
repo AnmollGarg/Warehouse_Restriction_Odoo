62e4de9c76cd5ebfb7d784d18107acd82fc9f491
Anmol Garg <anmolgarg12203@gmail.com>
2025-09-16 12:58:19 +0530

    [Added] - Warehouse Allowed Operation Restriction

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,9 @@
 {
     'name': 'Warehouse Restriction',
     'version': '1.0',
-    'depends': ['base' , 'stock'],
+    'depends': ['base', 'stock', 'contacts', 'sale', 'account'],
     'data': [
+        'security/warehouse_restriction_rules.xml',
         'views/res_users_view.xml',
     ],
     'installable': True,
--- models/res_users.py
+++ models/res_users.py
@@ -1,13 +1,28 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    restricted_warehouse = fields.Boolean(string='Restricted Warehouse')
+    restricted_warehouse = fields.Boolean(string='Restrict Warehouse Operations')
 
-    #Preferences
-    default_warehouse_operations = fields.Many2one('stock.warehouse', string='Default Warehouse Operations')
-    restrict_location = fields.Boolean(string='Restrict Location')
+    # Preferences
+    default_warehouse_operations = fields.Many2many(
+        'stock.picking.type', 
+        string='Allowed Operations',
+        help='User will only be able to access these operations'
+    )
+    restrict_location = fields.Boolean(string='Restrict Locations')
 
-    #Allowed Stock Locations
-    allowed_locations = fields.Many2many('stock.location', string='Allowed Stock Locations')
+    # Allowed Stock Locations
+    allowed_locations = fields.Many2many(
+        'stock.location',
+        string='Allowed Stock Locations',
+        help='User will only be able to access these stock locations'
+    )
+
+    @api.constrains('restricted_warehouse', 'default_warehouse_operations')
+    def _check_default_warehouse_operations(self):
+        for user in self:
+            if user.restricted_warehouse and not user.default_warehouse_operations:
+                raise ValidationError(self.env._("You must select at least one allowed operation if 'Restrict Warehouse Operations' is enabled."))
